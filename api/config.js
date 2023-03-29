30c833d068971383a7285d176f961e31cb6c696d
Hernan Nicolas Varela <hernanvarela45gmail.com>
2023-03-28 23:03:53 -0300

    Se creo la base de datos(api/src/db.js)

--- api/config.js
+++ api/config.js
@@ -2,6 +2,12 @@
 const config = {
     appConfig : {
         port : process.env.APP_PORT
+    },
+
+    dbConfig : {
+        port : process.env.DB_PORT,
+        host : process.env.DB_HOST,
+        dbName : process.env.DB_NAME
     }
 }
 
--- api/index.js
+++ api/index.js
@@ -20,14 +20,18 @@
 
    require('dotenv').config() // permite usar las variables de entorno
    server = require('./src/app.js');
-   const {appConfig} = require('./config')
+   const connectDb = require('./src/db')
+   const {appConfig, dbConfig} = require('./config')
 
+   async function initApp(app_config, db_config){
+     try {
+      await connectDb(db_config)
+      server.listen(app_config.port, () => console.log(`Server listen on port:  ${app_config.port}`)); // eslint-disable-line no-console
+     } catch (e) {
+      console.error(e)
+      process.exit(0) // si hay un error al pasarle 0 corta el proceso de node
+     }
+  }
 
-// const { conn } = require('./src/db.js');
+  initApp(appConfig,dbConfig)
 
-// Syncing all the models at once.
-// conn.sync({ force: true }).then(() => {
-  server.listen(appConfig.port, () => {
-    console.log(`Server listen on port:  ${appConfig.port}`); // eslint-disable-line no-console
-  });
-// });
