55a376ae89e019a5c73d5887b12a5a3829b1086f
Darwin Roa <darwinmichaelroamora@gmail.com>
2023-03-29 20:54:52 -0500

    Componentw CardReview creado, este componente es la plantilla que sirve para mostrar el comentario creado por un usuario cliente

---
+++ client/src/components/CardReview.jsx
@@ -0,0 +1,26 @@
+import React from "react";
+
+export default function CardReview() {
+  return (
+    <div>
+      <div>
+        <div>
+          <img
+            src="https://www.google.com/url?sa=i&url=https%3A%2F%2Fpngtree.com%2Ffree-png-vectors%2Fuser-avatar&psig=AOvVaw0sHRdC6wbm5y8z4Dcb1or4&ust=1680226532397000&source=images&cd=vfe&ved=0CBAQjRxqFwoTCKDZzN7Bgv4CFQAAAAAdAAAAABAd"
+            alt="image review"
+          />
+        </div>
+        <div>⭐⭐⭐⭐⭐</div>
+      </div>
+      <div>
+        <h3>Floyd Miles</h3>
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Iure enim,
+          obcaecati reprehenderit exercitationem animi modi consectetur vitae
+          esse quam ab nisi adipisci impedit explicabo repudiandae, autem ipsam,
+          tempore suscipit. Inventore!
+        </p>
+      </div>
+    </div>
+  );
+}
