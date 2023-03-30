25db3e3f22cf9c75280af921f11e5bf753cc0fe2
Hernan Nicolas Varela <hernanvarela45gmail.com>
2023-03-30 13:56:38 -0300

    mejoras en el schema usuario

--- api/src/db.js
+++ api/src/db.js
@@ -4,7 +4,11 @@ const mongoose = require('mongoose')
 mongoose.connection.on('open', () => console.log('db connected '))
 
 async function connectDb({ uri }) {
-    await mongoose.connect( uri, {useNewUrlParser : true})
+    try {
+        await mongoose.connect( uri, {useNewUrlParser : true})
+    } catch (error) {
+        console.log({error : error.message})
+    }
 }
 
 module.exports = connectDb
--- api/src/models/Usuario.js
+++ api/src/models/Usuario.js
@@ -2,6 +2,19 @@ const mongoose = require("mongoose");
 
 const Schema = mongoose.Schema;
 
+const ComentSchema = Schema(
+  {
+    text: String,
+    rating: Number,
+    userId: {
+      type: Schema.Types.ObjectId,
+      ref: "Usuario"
+    }
+  },
+  { timestamps: true }
+);
+
+
 const UsuarioSchema = Schema(
   {
     fullName: String,
@@ -11,6 +24,7 @@ const UsuarioSchema = Schema(
     phone: String,
     status: Boolean,
     type: String,
+    coments: [ComentSchema],
   },
   { timestamps: true }
 );
