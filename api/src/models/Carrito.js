2a55f73c8c6faafb27d05846479bcd4201398fd5
GeovannyManga <geovannyx.casimirom@unac.edu.co>
2023-03-31 12:25:29 -0500

    hola mundo

--- api/src/models/Carrito.js
+++ api/src/models/Carrito.js
@@ -5,7 +5,7 @@ const mongoose = require("mongoose")
 const cartSchema = new mongoose.Schema({
     user: { type: Object, ref: 'User' },
     rooms: [{ type: Object, ref: 'Room' }],
-    transport: { type: Object, ref: 'Transport' },
+    transport:[{ type: Object, ref: 'Transport' }],
   });
 
   module.exports = mongoose.model('Cart', cartSchema);
