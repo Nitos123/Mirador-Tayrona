2b764599a3902555c33fd03ed9bc3d6c089b33b8
jkapa7 <juandaniel5005@gmail.com>
2023-03-30 23:16:52 -0500

    Trabaje el diseño para que se vean dos columnas en about, y en la info del principio

--- client/src/components/About.jsx
+++ client/src/components/About.jsx
@@ -1,26 +1,34 @@
 import React from "react";
+import "../styles/About.scss";
 
 const About = () => {
   return (
     <div>
-      <h1>About us</h1>
-      <h3>
-        Lorem ipsum dolor sit amet consectetur, adipisicing elit. Officiis quasi
-        id molestiae cumque labore dolore quos in accusantium, similique
-        incidunt rem neque sapiente eum. Quibusdam laborum obcaecati error
-        provident fugit.Lorem ipsum dolor sit amet consectetur, adipisicing
-        elit. Officiis quasi id molestiae cumque labore dolore quos in
-        accusantium, similique incidunt rem neque sapiente eum. Quibusdam
-        laborum obcaecati error provident fugit.Lorem ipsum dolor sit amet
-        consectetur, adipisicing elit. Officiis quasi id molestiae cumque labore
-        dolore quos in accusantium, similique incidunt rem neque sapiente eum.
-        Quibusdam laborum obcaecati error provident fugit.
-      </h3>
-      <div>
-        <img
-          src="     https://blog.advesa.com/wp-content/uploads/2019/11/about-us-page.png"
-          alt="image review"
-        />
+      <div className="sectionAbout">
+        <h1>About us</h1>
+        <div className="aboutContainer">
+          <div>
+            <h3>
+              Lorem ipsum dolor sit amet consectetur, adipisicing elit. Officiis
+              quasi id molestiae cumque labore dolore quos in accusantium,
+              similique incidunt rem neque sapiente eum. Quibusdam laborum
+              obcaecati error provident fugit.Lorem ipsum dolor sit amet
+              consectetur, adipisicing elit. Officiis quasi id molestiae cumque
+              labore dolore quos in accusantium, similique incidunt rem neque
+              sapiente eum. Quibusdam laborum obcaecati error provident
+              fugit.Lorem ipsum dolor sit amet consectetur, adipisicing elit.
+              Officiis quasi id molestiae cumque labore dolore quos in
+              accusantium, similique incidunt rem neque sapiente eum. Quibusdam
+              laborum obcaecati error provident fugit.
+            </h3>
+          </div>
+          <div>
+            <img
+              src="     https://blog.advesa.com/wp-content/uploads/2019/11/about-us-page.png"
+              alt="image review"
+            />
+          </div>
+        </div>
       </div>
     </div>
   );
--- client/src/views/Home.jsx
+++ client/src/views/Home.jsx
@@ -12,106 +12,127 @@ const Home = () => {
         <h1>Estoy en el home</h1>
       </div>
 
-      <div className="txtUnderMainImage">
-        <p>Restorant</p>
-        <p>Garage</p>
-        <p>Resort</p>
-        <p>Free Wifi</p>
-      </div>
+      <section className="section">
+        <div className="txtUnderMainImage">
+          <p>Restorant</p>
+          <p>Garage</p>
+          <p>Resort</p>
+          <p>Free Wifi</p>
+        </div>
 
-      <h1>Why You Should Stay Here</h1>
-      <p>
-        Lorem ipsum dolor sit amet consectetur, adipisicing elit. Ipsam alias
-        suscipit saepe officia sed labore enim doloribus molestiae! Magni
-        facilis possimus unde fuga, in expedita quibusdam sunt quae veritatis
-        harum!
-      </p>
+        <div>
+          <h1>Why You Should Stay Here</h1>
+          <p>
+            Lorem ipsum dolor sit amet consectetur, adipisicing elit. Ipsam
+            alias suscipit saepe officia sed labore enim doloribus molestiae!
+            Magni facilis possimus unde fuga, in expedita quibusdam sunt quae
+            veritatis harum!
+          </p>
+        </div>
 
-      <div className="stayHere">
-        <div className="stayHereTxt">
-          <div>
-            <p>Provide the best choice of Room.</p>
-            <p>
-              Lorem ipsum dolor sit amet consectetur adipisicing elit. Tempore
-              neque in dicta ipsa maiores. Adipisci dolore veritatis numquam
-              officiis incidunt esse corrupti eaque soluta, eos vitae omnis
-              sint? Doloremque, iste.
-            </p>
-          </div>
+        <div>
+          <div className="stayHere">
+            <div className="stayHereTxt">
+              <div>
+                <p>Provide the best choice of Room.</p>
+                <p>
+                  Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                  Tempore neque in dicta ipsa maiores. Adipisci dolore veritatis
+                  numquam officiis incidunt esse corrupti eaque soluta, eos
+                  vitae omnis sint? Doloremque, iste.
+                </p>
+              </div>
 
-          <div>
-            <p>Low price with Best Quality</p>
-            <p>
-              Lorem ipsum dolor sit amet consectetur adipisicing elit. Tempore
-              neque in dicta ipsa maiores. Adipisci dolore veritatis numquam
-              officiis incidunt esse corrupti eaque soluta, eos vitae omnis
-              sint? Doloremque, iste.
-            </p>
-          </div>
+              <div>
+                <p>Low price with Best Quality</p>
+                <p>
+                  Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                  Tempore neque in dicta ipsa maiores. Adipisci dolore veritatis
+                  numquam officiis incidunt esse corrupti eaque soluta, eos
+                  vitae omnis sint? Doloremque, iste.
+                </p>
+              </div>
 
-          <div>
-            <p>Restaurant Service</p>
-            <p>
-              Lorem ipsum dolor sit amet consectetur adipisicing elit. Tempore
-              neque in dicta ipsa maiores. Adipisci dolore veritatis numquam
-              officiis incidunt esse corrupti eaque soluta, eos vitae omnis
-              sint? Doloremque, iste.
-            </p>
+              <div>
+                <p>Restaurant Service</p>
+                <p>
+                  Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                  Tempore neque in dicta ipsa maiores. Adipisci dolore veritatis
+                  numquam officiis incidunt esse corrupti eaque soluta, eos
+                  vitae omnis sint? Doloremque, iste.
+                </p>
+              </div>
+            </div>
+
+            <div className="stayHereImg">
+              <img
+                src="https://www.kayak.es/rimg/kimg/c0/f2/74274b63-61d6fbf1-27.jpeg?width=1366&height=768&crop=true"
+                alt="image review"
+              />
+            </div>
           </div>
         </div>
-        <div className="stayHereImg">
-          <img
-            src="https://www.kayak.es/rimg/kimg/c0/f2/74274b63-61d6fbf1-27.jpeg?width=1366&height=768&crop=true"
-            alt="image review"
-          />
+      </section>
+
+      <div className="section">
+        <h1>Take a look At our Rooms</h1>
+        <p>
+          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusamus
+          suscipit, animi officia quo, culpa minus sequi voluptas ad vitae
+          veritatis voluptate dolores possimus vel omnis dicta perferendis? Ut,
+          minus cum.
+        </p>
+        <div>
+          <CardRoomContainer />
         </div>
-      </div>
-
-      <h1>Take a look At our Rooms</h1>
-      <p>
-        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusamus
-        suscipit, animi officia quo, culpa minus sequi voluptas ad vitae
-        veritatis voluptate dolores possimus vel omnis dicta perferendis? Ut,
-        minus cum.
-      </p>
-
-      <div>
-        <CardRoomContainer />
       </div>
 
       <section id="About">
         <About />
       </section>
 
-      <div className="txtUnderMainImage">
-        <h2>Restorant</h2>
-        <p>
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Molestias
-          odit ex, qui quo recusandae hic quasi ratione explicabo consequatur.
-          Nobis dolor temporibus aperiam facilis totam obcaecati distinctio
-          mollitia! Id, ducimus?
-        </p>
-        <h2>Garage</h2>
-        <p>
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Molestias
-          odit ex, qui quo recusandae hic quasi ratione explicabo consequatur.
-          Nobis dolor temporibus aperiam facilis totam obcaecati distinctio
-          mollitia! Id, ducimus?
-        </p>
-        <h2>Resort</h2>
-        <p>
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Molestias
-          odit ex, qui quo recusandae hic quasi ratione explicabo consequatur.
-          Nobis dolor temporibus aperiam facilis totam obcaecati distinctio
-          mollitia! Id, ducimus?
-        </p>
-        <h2>Free Wifi</h2>
-        <p>
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Molestias
-          odit ex, qui quo recusandae hic quasi ratione explicabo consequatur.
-          Nobis dolor temporibus aperiam facilis totam obcaecati distinctio
-          mollitia! Id, ducimus?
-        </p>
+      <div className="section">
+        <div className="txtUnderMainImage">
+          <div>
+            <h2>Restorant</h2>
+            <p>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Molestias
+              odit ex, qui quo recusandae hic quasi ratione explicabo
+              consequatur. Nobis dolor temporibus aperiam facilis totam
+              obcaecati distinctio mollitia! Id, ducimus?
+            </p>
+          </div>
+
+          <div>
+            <h2>Garage</h2>
+            <p>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Molestias
+              odit ex, qui quo recusandae hic quasi ratione explicabo
+              consequatur. Nobis dolor temporibus aperiam facilis totam
+              obcaecati distinctio mollitia! Id, ducimus?
+            </p>
+          </div>
+
+          <div>
+            <h2>Resort</h2>
+            <p>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Molestias
+              odit ex, qui quo recusandae hic quasi ratione explicabo
+              consequatur. Nobis dolor temporibus aperiam facilis totam
+              obcaecati distinctio mollitia! Id, ducimus?
+            </p>
+          </div>
+
+          <div>
+            <h2>Free Wifi</h2>
+            <p>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Molestias
+              odit ex, qui quo recusandae hic quasi ratione explicabo
+              consequatur. Nobis dolor temporibus aperiam facilis totam
+              obcaecati distinctio mollitia! Id, ducimus?
+            </p>
+          </div>
+        </div>
       </div>
 
       <div>
