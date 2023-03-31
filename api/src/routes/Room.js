96954d5fd0c65eb2008fd6525eb34ef13af88cdc
N1t4z0 <114629649+N1t4z0@users.noreply.github.com>
2023-03-31 17:49:46 -0300

    add routes get roomsById

--- api/src/routes/Room.js
+++ api/src/routes/Room.js
@@ -1,10 +1,11 @@
 const {Router}= require("express")
 
-const { addRoom, getAllRooms  } = require("../../controller/roomController")
+const { addRoom, getAllRooms, getRoomId  } = require("../../controller/roomController")
 
 const api = Router()
 
 api.post("/room", addRoom)
 api.get("/room", getAllRooms)
+api.get("/room/:id", getRoomId )
 
 module.exports = api;
