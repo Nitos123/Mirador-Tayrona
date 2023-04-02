4f6b5ed029314d37a2e8bc266f679d4c03b4af62
Darwin Roa <darwinmichaelroamora@gmail.com>
2023-04-02 18:29:09 -0500

    Agregado un botón para resetear los filtros

--- client/src/views/Rooms.jsx
+++ client/src/views/Rooms.jsx
@@ -3,7 +3,7 @@ import CardRoomContainer from "../components/CardsRoomContainer";
 import CardServicesContainer from "../components/CardsServicesContainer";
 import "../styles/Rooms.scss";
 import { useDispatch } from "react-redux";
-import { getMaxPrice, getMinPrice, getType } from "../redux/actions";
+import { getMaxPrice, getMinPrice, getType, reset } from "../redux/actions";
 
 const rooms = (props) => {
   const dispatch = useDispatch();
@@ -52,7 +52,8 @@ const rooms = (props) => {
 
         <section className="roomsFilters">
           <div>
-            <select onChange={(e) => handlerMaxPrce(e)}>
+            <select defaultValue={"Price"} onChange={(e) => handlerMaxPrce(e)}>
+              <option disabled value="Price">Price</option>
               <option value="maxPrice">Price maximo</option>
               <option value="minPrice">Price minimo</option>
             </select>
@@ -65,6 +66,7 @@ const rooms = (props) => {
               <option value="individual">Individual</option>
               <option value="familiar">Familiar</option>
             </select>
+            <button onClick={() => dispatch(reset())}>Reset</button>
             {/* <select>
               <option>Date</option>
             </select> */}
@@ -84,15 +86,3 @@ const rooms = (props) => {
 };
 
 export default rooms;
-
-// const filteredProducts = products.filter((product) => {
-//   const price = parseFloat(product.price);
-//   if (minPrice && maxPrice) {
-//     return price >= parseFloat(minPrice) && price <= parseFloat(maxPrice);
-//   } else if (minPrice) {
-//     return price >= parseFloat(minPrice);
-//   } else if (maxPrice) {
-//     return price <= parseFloat(maxPrice);
-//   }
-//   return true;
-// })
