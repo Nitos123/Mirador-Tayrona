601a9519e040dc89770092115cc7bdcc96332704
GeovannyManga <geovannyx.casimirom@unac.edu.co>
2023-04-02 02:14:50 -0500

    ruta detail arreglada en el front y carrucel de imagenes listo

--- client/src/views/Detail.jsx
+++ client/src/views/Detail.jsx
@@ -7,33 +7,39 @@ import { getRoomDetail } from "../redux/actions";
 const Detail = (props) => {
   const detail = useSelector((state) => state.detail);
   const dispatch = useDispatch();
-  const id = useParams();
+  const id = useParams().id;
 
   useEffect(() => {
     dispatch(getRoomDetail(id));
-    console.log(getRoomDetail(id));
+    console.log(id);
   }, [dispatch, id]);
 
   console.log(detail);
+
+  const style = {
+    height: "300px",
+    width: "100%",
+  }
 
   return (
     <div>
       <div className="mainImageRooms"></div>
       <div className="section">
         <h1>Estoy en Detail</h1>
+        {detail ? (
         <div>
-          <p>{detail.desctiption}</p>
-
-          <img src={detail.image} alt="image review" />
-
+          <p>{detail?.desctiption}</p>
+          <img style={style} src={detail.image} alt="image review" />
           <button>Book this room!</button>
-
           <h1>More rooms</h1>
           <CardRoomContainer />
         </div>
-      </div>
+      ) : (
+        <p>Cargando información...</p>
+      )}
     </div>
-  );
+  </div>
+);
 };
 
 export default Detail;
