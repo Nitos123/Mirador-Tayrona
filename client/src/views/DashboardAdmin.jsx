3bea11dd861c0b71694a580298a0ed073804f0b1
jkapa7 <juandaniel5005@gmail.com>
2023-04-12 16:35:25 -0500

    Diseño del dashboard alineado con el diseño de la app global, cree los estilos de dashboardAdmin

--- client/src/views/DashboardAdmin.jsx
+++ client/src/views/DashboardAdmin.jsx
@@ -1,97 +1,74 @@
-import clsx from "clsx";
-import Drawer from "@material-ui/core/Drawer";
-import AppBar from "@material-ui/core/AppBar";
-import Toolbar from "@material-ui/core/Toolbar";
-import List from "@material-ui/core/List";
-import Typography from "@material-ui/core/Typography";
 import IconButton from "@material-ui/core/IconButton";
 import Badge from "@material-ui/core/Badge";
-import Avatar from "@material-ui/core/Avatar";
 import NotificationsIcon from "@material-ui/icons/Notifications";
-import useStyles from "./styles";
 import React from "react";
-import ListItem from "@material-ui/core/ListItem";
-import ListItemIcon from "@material-ui/core/ListItemIcon";
-import ListItemText from "@material-ui/core/ListItemText";
 import dashboard from "../assets/dashboard.svg";
 import user from "../assets/user.svg";
-import wallet from "../assets/Wallet.svg";
 import customer from "../assets/customer.svg";
 import { Link } from "react-router-dom";
+import "../styles/DashboardAdmin.scss";
 
-const mainListItems = (
-  <>
-    <ListItem button active>
-      <ListItemIcon>
-        <img src={dashboard} alt="dashboard" />
-      </ListItemIcon>
-      <ListItemText className="list_item_text" primary="Rooms" />
-    </ListItem>
-
-    <ListItem button>
-      <ListItemIcon>
-        <img src={customer} alt="wallet" />
-      </ListItemIcon>
-      <ListItemText className="list_item_text" primary="Users" />
-    </ListItem>
-
-    <ListItem button>
-      <ListItemIcon>
-        <img src={wallet} alt="activity" />
-      </ListItemIcon>
-      <ListItemText className="list_item_text" primary="Reviews" />
-    </ListItem>
-
-    <div>
-      <Link to="/home">
-        <button>Back Home</button>
-      </Link>
-    </div>
-  </>
-);
-
-const DashboardAdmin = () => {
-  const classes = useStyles();
-
+const MainListItems = () => {
   return (
-    <div className={classes.root}>
-      <AppBar
-        position="absolute"
-        className={clsx(classes.appBar, open && classes.appBarShift)}
-      >
-        <Toolbar className={classes.toolbar}>
-          <Typography
-            component="h1"
-            variant="h6"
-            color="#000"
-            noWrap
-            className={classes.title}
-          >
-            Balances
-          </Typography>
-          <IconButton color="inherit">
-            <Badge badgeContent={4} color="secondary">
-              <NotificationsIcon />
-            </Badge>
-          </IconButton>
-          <Avatar alt="Remy Sharp" id="user_img" src={user} />
-        </Toolbar>
-      </AppBar>
+    <div className="left-title">
+      <div>
+        <h3>Mirador Tayrona Park</h3>
+      </div>
 
-      <Drawer
-        variant="permanent"
-        classes={{
-          paper: clsx(classes.drawerPaper, !open && classes.drawerPaperClose),
-        }}
-        open={open}
-        id="mainDrawer"
-      >
-        <div className="draw_bar">
-          <h3>Mirador Tayrona Park</h3>
+      <div>
+        <div>
+          <Link>
+            <img src={dashboard} alt="activity" />
+            Rooms
+          </Link>
         </div>
 
-        <List>{mainListItems}</List>
-      </Drawer>
+        <div>
+          <Link>
+            <img src={customer} alt="activity" />
+            Users
+          </Link>
+        </div>
+
+        <div>
+          <Link>
+            <img src={dashboard} alt="activity" />
+            Reviews
+          </Link>
+        </div>
+      </div>
+
+      <div>
+        <Link to="/home">
+          <button>Back Home</button>
+        </Link>
+      </div>
+    </div>
+  );
+};
+
+const DashboardAdmin = () => {
+  return (
+    <div className="main-container-admin">
+      <div className="left-container">
+        <MainListItems />
+      </div>
+
+      <div className="rg-container">
+        <div>
+          <h3>Rooms</h3>
+        </div>
+
+        <div>
+          <Badge badgeContent={5} color="secondary">
+            <NotificationsIcon />
+          </Badge>
+        </div>
+
+        <div>
+          <img alt="Remy Sharp" id="user_img" src={user} />
+        </div>
+      </div>
     </div>
   );
 };
