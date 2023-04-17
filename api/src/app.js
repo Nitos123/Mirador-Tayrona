7d912857f9ed2b029e240562b1c62a1fccc7b87d
jkapa7 <juandaniel5005@gmail.com>
2023-04-17 14:11:11 -0500

    Modularice la parte del back de stripe. las cors se ven afectadas porque el llamado sucedia antres de defiinir las cors, por lo que lo modularice cree la ruta y desde ahi lo trabajo, ya funciona

--- api/src/app.js
+++ api/src/app.js
@@ -7,83 +7,12 @@ const routes = require("./routes/index.js");
 
 //
 
-/////////////////////////
-// REQUERIDO PARA QUE LA PASARELA DE PAGO FUNCIONE
-const { resolve } = require("path");
-const env = require("dotenv").config({ path: "./.env" });
-const stripe = require("stripe")(process.env.STRIPE_SECRET_KEY, {
-  apiVersion: "2022-08-01",
-});
-
 require("./db.js");
 
 const server = express();
 server.use(express.static("public"));
 server.name = "API";
 server.use(express.json());
-
-///////////////////////
-//SON LAS RUTAS PARA LA PARTE DE LA PARESALE DE PAGOS
-server.use(express.static(process.env.STATIC_DIR));
-
-server.get("/nada", (req, res) => {
-  const path = resolve(process.env.STATIC_DIR + "/index.html");
-  res.sendFile(path);
-});
-
-server.get("/config", (req, res) => {
-  res.send({
-    publishableKey: process.env.STRIPE_PUBLISHABLE_KEY,
-  });
-});
-
-server.post("/create-payment-intent", async (req, res) => {
-  try {
-    const paymentIntent = await stripe.paymentIntents.create({
-      currency: "usd",
-      amount: 1999,
-      automatic_payment_methods: {
-        enabled: true,
-      },
-    });
-
-    res.send({ clientSecret: paymentIntent.client_secret });
-  } catch (error) {
-    return res.status(400).send({
-      error: {
-        message: error.message,
-      },
-    });
-  }
-});
-/////////////////////////////
-
-//
-
-server.post("/api/checkout", async (req, res) => {
-  console.log(req.body);
-
-  try {
-    const { id, amount } = req.body;
-
-    const payment = await stripe.paymentIntents.create({
-      amount,
-      currency: "USD",
-      description: "",
-      payment_method: id,
-      confirm: true,
-    });
-
-    console.log(payment);
-
-    res.send({ message: "Succesfull payment" });
-  } catch (error) {
-    console.log(error);
-
-    res.json({ message: error.message });
-  }
-});
-//
 
 //
 
@@ -97,10 +26,10 @@ server.use(cookieParser());
 server.use(morgan("dev"));
 server.use((req, res, next) => {
   // res.header("Access-Control-Allow-Origin", "http://localhost:3000");
-  res.header("Access-Control-Allow-Origin", "*"); // update to match the domain you will make the request from
-  // res.header("Access-Control-Allow-Origin", "http://127.0.0.1:5173");
+  // res.header("Access-Control-Allow-Origin", "*");
+  // update to match the domain you will make the request from
+  res.header("Access-Control-Allow-Origin", "http://127.0.0.1:5173");
 
-  // res.header("Access-Control-Allow-Origin", "http://localhost:5173"); // update to match the domain you will make the request from
   res.header("Access-Control-Allow-Credentials", "true");
   res.header(
     "Access-Control-Allow-Headers",
--- client/src/components/CheckoutForm.jsx
+++ client/src/components/CheckoutForm.jsx
@@ -1,7 +1,17 @@
+import { React, useState, useEffect } from "react";
+import { useSelector, useDispatch } from "react-redux";
 import "../styles/Checkout.scss";
 import { loadStripe } from "@stripe/stripe-js";
+import axios from "axios";
 import "../styles/Checkout.scss";
-import { Elements } from "@stripe/react-stripe-js";
+import {
+  Elements,
+  CardElement,
+  useStripe,
+  useElements,
+} from "@stripe/react-stripe-js";
+import { restoreCartFromLocalStorage, carritoUser } from "../redux/actions";
+import { useAuth } from "../context/authContext";
 import "../styles/CheckoutForm.scss";
 import Payment from "./Payment";
 
@@ -9,133 +19,134 @@ const stripePromise = loadStripe(
   "pk_test_51MtdRJAxd88LZv2eI3ZXSTGWh0VL8z8i799gIye6ke36gZzmc7H73kJvKRmgW7msmfdIhz0VwCql9Koq7WdGo3Zg009lR7Uc3t"
 );
 
-// const StripeForm = (props) => {
-//   const stripe = useStripe();
-//   const { user } = useAuth();
-//   const dispatch = useDispatch();
+const StripeForm = (props) => {
+  const stripe = useStripe();
+  const { user } = useAuth();
+  const dispatch = useDispatch();
 
-//   const elements = useElements();
+  const elements = useElements();
 
-//   const carrito = useSelector((state) => state.carrito);
-//   const [loading, setLoading] = useState(false);
+  const carrito = useSelector((state) => state.carrito);
+  const [loading, setLoading] = useState(false);
 
-//   let totalPrice = 0;
+  let totalPrice = 0;
 
-//   for (let i = 0; i < carrito.length; i++) {
-//     totalPrice += carrito[i].price;
-//   }
+  for (let i = 0; i < carrito.length; i++) {
+    totalPrice += carrito[i].price;
+  }
 
-//   useEffect(() => {
-//     dispatch(restoreCartFromLocalStorage("carrito"));
-//     if (user && user.email) {
-//       const userMail = user.email;
-//       dispatch(carritoUser(userMail));
-//     }
-//   }, [dispatch]);
+  useEffect(() => {
+    dispatch(restoreCartFromLocalStorage("carrito"));
+    if (user && user.email) {
+      const userMail = user.email;
+      dispatch(carritoUser(userMail));
+    }
+  }, [dispatch]);
 
-//   const handleSubmit = async (event) => {
-//     event.preventDefault();
+  const handleSubmit = async (event) => {
+    event.preventDefault();
 
-//     const { error, paymentMethod } = await stripe.createPaymentMethod({
-//       type: "card",
-//       card: elements.getElement(CardElement),
-//     });
-//     setLoading(true);
+    const { error, paymentMethod } = await stripe.createPaymentMethod({
+      type: "card",
+      card: elements.getElement(CardElement),
+    });
+    setLoading(true);
 
-//     if (!error) {
-//       const { id } = paymentMethod;
+    if (!error) {
+      const { id } = paymentMethod;
 
-//       try {
-//         const { data } = await axios.post("/api/checkout", {
-//           id,
-//           amount: 10 * 100,
-//         });
+      try {
+        const { data } = await axios.post("/api/checkout", {
+          id,
+          amount: 10 * 100,
+        });
 
-//         elements.getElement(CardElement).clear();
-//       } catch (error) {}
-//       setLoading(false);
-//     }
-//   };
+        elements.getElement(CardElement).clear();
+      } catch (error) {}
+      setLoading(false);
+    }
+  };
 
-//   return (
-//     <div className="form-container">
-//       <form onSubmit={handleSubmit}>
-//         <div>
-//           <h3>Card Details</h3>
-//         </div>
+  return (
+    <div className="form-container">
+      <form onSubmit={handleSubmit}>
+        <div>
+          <h3>Card Details</h3>
+        </div>
 
-//         <div className="products-container">
-//           <div>
-//             {carrito?.map((carro) => {
-//               return <div> {carro.name}</div>;
-//             })}
-//           </div>
+        <div className="products-container">
+          <div>
+            {carrito?.map((carro) => {
+              return <div> {carro.name}</div>;
+            })}
+          </div>
 
-//           <div>
-//             {carrito?.map((carro) => {
-//               return <div> {carro.price}</div>;
-//             })}
-//           </div>
-//         </div>
+          <div>
+            {carrito?.map((carro) => {
+              return <div> {carro.price}</div>;
+            })}
+          </div>
+        </div>
 
-//         <h3>Card types</h3>
-//         <div>
-//           <div className="cards-image">
-//             <div>
-//               <img
-//                 className="image-checkout"
-//                 src="https://res.cloudinary.com/ds8n6d63e/image/upload/v1681423455/Hostel-tayrona/2560px-Visa_Inc._logo.svg_zjh3rt.png"
-//               />
-//             </div>
+        <h3>Card types</h3>
+        <div>
+          <div className="cards-image">
+            <div>
+              <img
+                className="image-checkout"
+                src="https://res.cloudinary.com/ds8n6d63e/image/upload/v1681423455/Hostel-tayrona/2560px-Visa_Inc._logo.svg_zjh3rt.png"
+              />
+            </div>
 
-//             <div>
-//               <img
-//                 className="image-checkout"
-//                 src="https://res.cloudinary.com/ds8n6d63e/image/upload/v1681423482/Hostel-tayrona/mastercard_bank_money_card_20426_2048x1240_keb903.jpg"
-//               />
-//             </div>
+            <div>
+              <img
+                className="image-checkout"
+                src="https://res.cloudinary.com/ds8n6d63e/image/upload/v1681423482/Hostel-tayrona/mastercard_bank_money_card_20426_2048x1240_keb903.jpg"
+              />
+            </div>
 
-//             <div>
-//               <img
-//                 className="image-checkout"
-//                 src="https://res.cloudinary.com/ds8n6d63e/image/upload/v1681423506/Hostel-tayrona/2560px-RuPay.svg_x80o6e.png"
-//               />
-//             </div>
-//           </div>
-//         </div>
-//         <div className="form-group">
-//           <CardElement className="form-control" />
-//         </div>
-//         <div className="total-form">
-//           <div>
-//             <h4>Total</h4>
-//           </div>
+            <div>
+              <img
+                className="image-checkout"
+                src="https://res.cloudinary.com/ds8n6d63e/image/upload/v1681423506/Hostel-tayrona/2560px-RuPay.svg_x80o6e.png"
+              />
+            </div>
+          </div>
+        </div>
+        <div className="form-group">
+          <CardElement className="form-control" />
+        </div>
+        <div className="total-form">
+          <div>
+            <h4>Total</h4>
+          </div>
 
-//           <div>
-//             <h3>{totalPrice}</h3>
-//           </div>
-//         </div>
-//         <div>
-//           <button className="btn btn-success" disabled={!stripe}>
-//             {loading ? (
-//               <div className="spinner-border text-light" role="status">
-//                 <span className="sr-only"></span>
-//               </div>
-//             ) : (
-//               "CHECKOUT"
-//             )}
-//           </button>
-//         </div>
-//       </form>
-//     </div>
-//   );
-//   d;
-// };
+          <div>
+            <h3>{totalPrice}</h3>
+          </div>
+        </div>
+        <div>
+          <button className="btn btn-success" disabled={!stripe}>
+            {loading ? (
+              <div className="spinner-border text-light" role="status">
+                <span className="sr-only"></span>
+              </div>
+            ) : (
+              "CHECKOUT"
+            )}
+          </button>
+        </div>
+      </form>
+    </div>
+  );
+  d;
+};
 
 const CheckoutForm = (props) => {
   return (
     <div>
       <Elements stripe={stripePromise}>
+        {/* <StripeForm /> */}
         <Payment />
       </Elements>
     </div>
