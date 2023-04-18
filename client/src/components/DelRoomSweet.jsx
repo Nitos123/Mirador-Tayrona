bdbe1aec15b7c75605e6f7877c1ca88d95fc84fb
hernan-varela <hernanvarela45@gmail.comm>
2023-04-18 20:16:19 -0300

    se le agrego un boton para eliminar las fotos individualmente  al momento de actializar una room

--- client/src/components/DelRoomSweet.jsx
+++ client/src/components/DelRoomSweet.jsx
@@ -1,44 +1,43 @@
 import Swal from "sweetalert2";
+import axios from "axios";
 
-const DelRoomSweet = (roomId) => {
-  return new Promise((resolve, reject) => {
-    Swal.fire({
+const DelRoomSweet = async (roomId) => {
+  try {
+    const result = await Swal.fire({
       title: "Are you sure?",
       text: "If you delete the room you will lose the data permanently!",
       icon: "warning",
       showCancelButton: true,
       confirmButtonColor: "#fd3131",
       confirmButtonText: "Yes, delete it!",
-    }).then((result) => {
-      if (result.isConfirmed) {
-        fetch(`/api/rooms/${roomId}`, {
-          method: "DELETE",
-        })
-          .then((response) => {
-            if (response.ok) {
-              Swal.fire({
-                icon: "success",
-                title: "Success!",
-                text: "Room deleted",
-              });
-              resolve(true);
-            } else {
-              throw new Error("Something went wrong");
-            }
-          })
-          .catch((error) => {
-            Swal.fire({
-              icon: "error",
-              title: "Error!",
-              text: "Room not deleted",
-            });
-            reject(false);
-          });
-      } else {
-        resolve(false);
-      }
     });
-  });
+    if (result.isConfirmed) {
+      console.log(roomId)
+      const response = await axios.put(`/update/room/${roomId}/status`, {
+        baseURL: "http://localhost:8080" // Establece la URL base del servidor aquí
+      });
+      if (response.status === 200) { // verifica el estado de la respuesta HTTP
+        await Swal.fire({
+          icon: "success",
+          title: "Success!",
+          text: "Room deleted",
+        });
+        return true;
+      } else {
+        throw new Error("Something went wrong");
+      }
+    } else {
+      return false;
+    }
+  } catch (error) {
+    console.error(error); // agrega console.error para mostrar el error en la consola
+    await Swal.fire({
+      icon: "error",
+      title: "Error!",
+      text: "Room not deleted",
+    });
+    return false;
+  }
 };
 
 export default DelRoomSweet;
