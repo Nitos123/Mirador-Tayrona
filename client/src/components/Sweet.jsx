742bdd6b36f329bb920d453c79ec46865c9619d1
GeovannyManga <geovannyx.casimirom@unac.edu.co>
2023-04-19 18:15:40 -0500

    nuevos cambios, carrito terminado al fin

--- client/src/components/Sweet.jsx
+++ client/src/components/Sweet.jsx
@@ -42,38 +42,6 @@ export const SweetAprovedPayment = (props) => {
     });
   });
 };
-return new Promise((resolve, reject) => {
-Swal.fire({
-title: "Payment Success",
-text: props,
-icon: "success",
-}).then((result) => {
-if (result.isConfirmed) {
-axios.delete('/ruta/para/borrar/items')
-.then(() => {
-Swal.fire({
-title: "Items removed from cart",
-text: "Your cart has been emptied.",
-icon: "success",
-});
-window.location.href = "/";
-resolve(true);
-})
-.catch((error) => {
-Swal.fire({
-title: "Error removing items from cart",
-text: "There was an error while removing items from your cart. Please try again later.",
-icon: "error",
-});
-console.log(error);
-reject(error);
-});
-} else {
-resolve(false);
-}
-});
-});
-};;
 
 export const SweetFailedReview = (props1, props2) => {
   return new Promise((resolve, reject) => {
