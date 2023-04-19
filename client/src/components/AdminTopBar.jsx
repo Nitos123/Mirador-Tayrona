cfb6c926c89ac5c7c863667393e4334186f13ad3
Darwin Roa <darwinmichaelroamora@gmail.com>
2023-04-19 16:45:10 -0500

    Solucionado Bug del paginado

--- client/src/components/AdminTopBar.jsx
+++ client/src/components/AdminTopBar.jsx
@@ -6,22 +6,21 @@ import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faUserTie, faBell } from "@fortawesome/free-solid-svg-icons";
 import { useSelector } from "react-redux";
 
-const TopBarAdmin = ({totPendRev}) => {
-
-  const count = useSelector(state => state.totalComments);
+const TopBarAdmin = ({ totPendRev }) => {
+  const count = useSelector((state) => state.totalComments);
   //aca quedaria usar la actionm
   const { user } = useAuth();
   return (
     <div className="rgd-container">
-      <div className="txt-part">
-        {/* <h3>{props.place}</h3> */}
-      </div>
+      <div className="txt-part">{/* <h3>{props.place}</h3> */}</div>
 
       <div className="notification-icon">
-        <FontAwesomeIcon icon={faBell} style={{  fontSize: '1.5rem', marginTop: '1rem '}} />
-        <span className="notification-count">{count}</span>
+        <FontAwesomeIcon
+          icon={faBell}
+          style={{ fontSize: "1.5rem", marginTop: "1rem " }}
+        />
+        {count > 0 ? <span className="notification-count">{count}</span> : ""}
       </div>
-
 
       <div className="user-part">
         {user && (
